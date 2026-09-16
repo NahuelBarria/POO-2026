21f9fb71ad9019c22b07366ae83be6e3f31e0c17
Nahuel <nahuuel.barrrria@gmail.com>
2026-09-16 18:40:05 -0300

    Ajedrez-V1

---
+++ TP02-Ajedrez/src/Casillero.java
@@ -0,0 +1,20 @@
+public class Casillero {
+    private String Color;
+    private int posicion;
+    public Casillero() {
+    }
+    public String getColor() {
+        return Color;
+    }
+    public void setColor(String color) {
+        Color = color;
+    }
+    public int getPosicion() {
+        return posicion;
+    }
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+
+    
+}
---
+++ TP02-Ajedrez/src/Tablero.java
@@ -0,0 +1,15 @@
+public class Tablero {
+    private String tablero [][] = new String[8][8];
+
+    public Tablero() {
+    }
+
+    public String[][] getTablero() {
+        return tablero;
+    }
+
+    public void setTablero(String[][] tablero) {
+        this.tablero = tablero;
+    }
+    
+}
